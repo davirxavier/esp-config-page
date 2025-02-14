c87a118d8644799908f0821e574ae29641b8a913
Davi Xavier <fdavirxxavier@gmail.com>
2025-02-14 15:03:02 -0300

    adjust folder structure

--- build.sh
+++
@@ -1,3 +0,0 @@
-./venv/bin/python updatehtml.py
-cd $PROJECT_PATH
-pio run -e $PIO_ENV
--- buildtool/updatehtml.py
+++ buildtool/updatehtml.py
@@ -1,7 +1,7 @@
 import minify_html
 import gzip
 
-with open('../config_page.html', encoding="utf-8") as f:
+with open('../include/config_page.html', encoding="utf-8") as f:
     read_data = f.read()
     minified = minify_html.minify(read_data, minify_css=True, minify_js=True, remove_bangs=True,
                                   remove_processing_instructions=True)
@@ -9,7 +9,7 @@ with open('../config_page.html', encoding="utf-8") as f:
 
 compressed = gzip.compress(minified.encode(), 9)
 
-with open('../config-html.h', 'w') as f:
+with open('../include/config-html.h', 'w') as f:
     f.write('''#ifndef DX_ESP_CONFIG_PAGE_HTML_H
 #define ESP_CONFIG_HTML_LEN %s
 #define DX_ESP_CONFIG_PAGE_HTML_H
