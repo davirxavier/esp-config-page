93797396ddb7ad5a1f2e8c3049c7dfef8e43789a
Davi Xavier <fdavirxxavier@gmail.com>
2026-02-20 00:00:00 +0000

    refactor project folders

--- buildtool/update_modules.py
+++ buildtool/update_modules.py
@@ -13,7 +13,7 @@ for module in modules:
 
 args = parser.parse_args()
 
-with open('../include/config_page.html', encoding="utf-8") as f:
+with open('./config_page.html', encoding="utf-8") as f:
     read_data = f.read()
 
     for e in args.__dict__:
@@ -27,7 +27,7 @@ with open('../include/config_page.html', encoding="utf-8") as f:
 
 compressed = gzip.compress(minified.encode(), 9)
 
-with open('../include/config-html.h', 'w') as f:
+with open('../src/config-html.h', 'w') as f:
     f.write('''#ifndef DX_ESP_CONFIG_PAGE_HTML_H
 #define ESP_CONFIG_HTML_LEN %s
 #define DX_ESP_CONFIG_PAGE_HTML_H
